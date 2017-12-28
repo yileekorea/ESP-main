ca3e855e68f3eacd9c4c5b214bee7f1d0dc51487
YONGIL LEE <yilee.korea@gmail.com>
2017-12-28 13:43:06 +0900

    if (mqttclient.publish((char *)topic_pub.c_str(), (char *)topic_pub.c_str())) { 			mqttclient.publish((char *)topic_pub.c_str(), "hello MQTT connected...");
    		if (mqttclient.publish((char *)topic_pub.c_str(), (char *)topic_pub.c_str())) {
    			mqttclient.publish((char *)topic_pub.c_str(), "hello MQTT connected...");

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -464,7 +464,9 @@ boolean mqtt_connect()
   if (mqttclient.connect(strID.c_str(), mqtt_user.c_str(), mqtt_pass.c_str())) {  // Attempt to connect
     DEBUG.println("MQTT connected");
 		// Publish
-		if (mqttclient.publish((char *)topic_pub.c_str(), "hello MQTT connected...")) {
+		//if (mqttclient.publish((char *)topic_pub.c_str(), "hello MQTT connected...")) {
+		if (mqttclient.publish((char *)topic_pub.c_str(), (char *)topic_pub.c_str())) {
+			mqttclient.publish((char *)topic_pub.c_str(), "hello MQTT connected...");
 			DEBUG.println("publish ok");
 		}
 		else {
