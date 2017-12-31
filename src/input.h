ac225b33def095c655e425779169e120beb49f9b
YONGIL LEE <yilee.korea@gmail.com>
2017-12-31 16:51:38 +0900

    #define interOpenTimer 400000UL //4min
    #define interOpenTimer 400000UL //4min

--- src/input.h
+++ src/input.h
@@ -27,7 +27,7 @@
 #define _EMONESP_INPUT_H
 
 #include <Arduino.h>
-#define interOpenTimer 300000UL //3min
+#define interOpenTimer 400000UL //4min
 
 // -------------------------------------------------------------------
 // Support for reading input
