a0ba8e5ec74ffa77a8518179f8a91741f4407755
YONGIL LEE <yilee.korea@gmail.com>
2017-12-31 21:55:35 +0900

    delay(100); added on all MQTT sending
    void sendTempData() {
        byte i;
        DEBUG.print("sendTempData numSensor : ");
        DEBUG.println(numSensor);
    
        for ( i = 0; i < numSensor ; i++) {
          send_a_TempData(i);
          delay(100);

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -225,6 +225,7 @@ void sendTempData() {
 
     for ( i = 0; i < numSensor ; i++) {
       send_a_TempData(i);
+      delay(100);
 /*
 		//if(old_celsius[i] != celsius[i]){
     //if(abs(old_celsius[i] - celsius[i]) > 0.1 )
