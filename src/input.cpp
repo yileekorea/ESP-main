535fa4b1cb85a5444d86158e9d1d543ee32d4cfe
YONGIL LEE <yilee.korea@gmail.com>
2018-01-13 09:49:13 +0900

    if((27.5 <= celsius[nSensor]) && ((millis() - Timer_2[nSensor]) > interOpenTimer) && (isOFF[nSensor] == 0)) {
         if((27.5 <= celsius[nSensor]) && ((millis() - Timer_2[nSensor]) > interOpenTimer) && (isOFF[nSensor] == 0)) {

--- src/input.cpp
+++ src/input.cpp
@@ -98,7 +98,7 @@ void INTsetup() {
    {
      //really going OFF condition...
      //if((L_Temp[nSensor] <= celsius[nSensor]) && ((millis() - Timer_2[nSensor]) > interOpenTimer) && (isOFF[nSensor] == 0)) {
-     if((28.2 <= celsius[nSensor]) && ((millis() - Timer_2[nSensor]) > interOpenTimer) && (isOFF[nSensor] == 0)) {
+     if((27.5 <= celsius[nSensor]) && ((millis() - Timer_2[nSensor]) > interOpenTimer) && (isOFF[nSensor] == 0)) {
        rStatus[nSensor] = 0;
        //Timer_1[nSensor] = millis();
        isOFF[nSensor] = 1;
