59789ad1773e04d85d1459e2de7ed63e3d73856d
KimYoungsook <kimyoungsook@YS-iMac.local>
2018-11-11 21:32:59 +0900

    ec2-fingerprint
    ec2-fingerprint

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -39,7 +39,8 @@
 #include <WiFiClientSecure.h>
 
 //sudo openssl x509 -in io2better.net.crt -sha1 -noout -fingerprint
-const char* fingerprint = "54:91:68:19:ED:B4:14:46:17:3F:69:48:EB:80:2F:60:2D:C6:83:0A";  //~~ 20181109 still using this print 
+//const char* fingerprint = "54:91:68:19:ED:B4:14:46:17:3F:69:48:EB:80:2F:60:2D:C6:83:0A";  //~~ 20181109 still using this print
+const char* fingerprint = "95:34:AF:46:C2:3B:D3:AB:C0:33:87:A7:1D:A6:82:AD:CA:FE:5B:AF";  //~~ 20181109 still using this print 
 long lastMqttReconnectAttempt = 0;
 int clientTimeout = 0;
 int i = 0;
