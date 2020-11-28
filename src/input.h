606e576b98b419c87dd725fe49cd7c8bbc24c850
yileekorea <yilee.korea@gmail.com>
2020-11-28 21:16:15 +0900

    define interOpenTimer 500000UL //5min

--- src/input.h
+++ src/input.h
@@ -27,8 +27,8 @@
 #define _EMONESP_INPUT_H
 
 #include <Arduino.h>
-//#define interOpenTimer 500000UL //5min
-#define interOpenTimer 400000UL //4min
+#define interOpenTimer 500000UL //5min
+//#define interOpenTimer 400000UL //4min
 //#define interOpenTimer 300000UL //3min
 
 // -------------------------------------------------------------------
