b813113bfc6706a006ef0c4e365d3a77c89a5464
yileekorea <yilee.korea@gmail.com>
2020-11-29 16:58:12 +0900

    //going ON condition      if((L_Temp[nSensor] > celsius[nSensor]) && ((now2 - Timer_1[nSensor]) > interOFF_Timer_30) && (isOFF[nSensor] == 1)) {

--- src/input.cpp
+++ src/input.cpp
@@ -114,7 +114,7 @@ void setON_OFFstatus(byte Sensor){
      }
 
      //going ON condition
-     if((L_Temp[nSensor] > celsius[nSensor]) && ((now2 - Timer_1[nSensor]) > interOFF_Timer_30)) {
+     if((L_Temp[nSensor] > celsius[nSensor]) && ((now2 - Timer_1[nSensor]) > interOFF_Timer_30) && (isOFF[nSensor] == 1)) {
        rStatus[nSensor] = L_Temp[nSensor];
        isOFF[nSensor] = 0;
        Timer_2[nSensor] = now2; //millis();   //point of turned ON
