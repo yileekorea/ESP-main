8ecf7aa15c5731ec2fadf29e83ef02635e6aa2c0
yileekorea <yilee.korea@gmail.com>
2020-11-29 18:41:10 +0900

    for(i=0;i<(numSensor-1);i++){     readoutTemperature(i);     if(L_Temp[i] <= celsius[i])  {          isOFF[i] = 0;        }        else{          isOFF[i] = 1;        }
        Timer_1[i] = now - interOFF_Timer_30; //point of turned OFF
        Timer_2[i] = now - interOpenTimer;    //point of turend ON
      }

--- src/ESP-main.ino
+++ src/ESP-main.ino
@@ -196,8 +196,16 @@ void setup() {
   }
 
   for(i=0;i<(numSensor-1);i++){
+    readoutTemperature(i);
+    if(L_Temp[i] <= celsius[i])  {
+         isOFF[i] = 0;
+       }
+       else{
+         isOFF[i] = 1;
+       }
+           
     Timer_1[i] = now - interOFF_Timer_30; //point of turned OFF
-    Timer_2[i] = now - interOpenTimer;    //point of turend ON    
+    Timer_2[i] = now - interOpenTimer;    //point of turend ON
   }
 
   SPIFFS2accHistory();
